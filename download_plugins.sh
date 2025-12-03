6386b15b40710c83ba81cc5c2e55f37f631b70cc
Valentina Feshina <valentina.feshina.qubership@gmail.com>
2025-12-03 11:20:58 +0300

    fix: return back Grafana AngularJS plugins (#53)
    * fix: return back Grafana AngularJS plugins
    Resolves #52
    
    * chore: fix linter issues
    
    * chore: fix linter issues
    
    * chore: fix linter issues
    
    * chore: fix linter issues

--- download_plugins.sh
+++ download_plugins.sh
@@ -8,6 +8,8 @@ REGISTRY_URL="https://grafana.com/api/plugins"
 TEMP_PATH="/tmp"
 DOWNLOADS_PATH="${TEMP_PATH}/downloads"
 DESTINATION_PATH="${TEMP_PATH}/plugins"
+RELEASE_VERSION="old-plugins"
+OLD_PLUGINS_RELEASE_URL="https://github.com/Netcracker/qubership-grafana-plugins-init/releases/download/${RELEASE_VERSION}"
 
 ######################################################################################################################
 #                                                  Download plugins                                                  #
@@ -17,21 +19,36 @@ mkdir -p ${DOWNLOADS_PATH} ${DESTINATION_PATH}
 
 echo "Start to read file with plugins ..."
 while IFS='' read -r line || [[ -n "${line}" ]]; do
-  if [[ "${line}" != \#* ]]; then
-    array=( ${line} )
+    [[ "$line" =~ ^# ]] && continue
 
+    read -r -a array <<<"$line"
     plugin_name=${array[0]}
     version=${array[1]}
 
     echo "Try to download plugin: ${plugin_name} - ${version}..."
     wget --no-check-certificate -P "${DOWNLOADS_PATH}/${plugin_name}" \
         -r -nd --quiet --no-parent \
-        "${REGISTRY_URL}/${plugin_name}/versions/${version}/download" \
+        "${REGISTRY_URL}/${plugin_name}/versions/${version}/download"
 
     unzip "${DOWNLOADS_PATH}/${plugin_name}/download" -d ${DESTINATION_PATH}
-  fi
-done < "plugins.list"
-echo "Plugins successfully downloaded"
+done <"plugins.list"
+echo "Official plugins successfully downloaded"
+
+######################################################################################################################
+#                                  Download old AngularJS plugins from old_plugins.list                              #
+######################################################################################################################
+
+echo "Start to download old AngularJS plugins..."
+while IFS='' read -r plugin || [[ -n "${plugin}" ]]; do
+    [[ "${plugin}" =~ ^# ]] && continue
+
+    echo "Downloading old plugin ${plugin} from GitHub Release..."
+    wget -q -O "${DOWNLOADS_PATH}/${plugin}.zip" \
+        "${OLD_PLUGINS_RELEASE_URL}/${plugin}.zip"
+    unzip -q "${DOWNLOADS_PATH}/${plugin}.zip" -d "${DESTINATION_PATH}"
+done <"old_plugins.list"
+
+echo "Old AngularJS plugins successfully downloaded"
 
 echo "Print downloaded plugins into directory:"
 ls -lah "${DOWNLOADS_PATH}"
